b0a84b435cd212041066c600546142eb0901ab63
zCoder <40376705+zchambers3@users.noreply.github.com>
2020-08-02 20:47:56 -0700

    Add files via upload

---
+++ Course Project/Course Project/Course Project/cell.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_Project
+{
+    class cell{
+
+        //Properties
+        public int columnX { get; set; }
+        public int rowY { get; set; }
+        public bool wasVisited { get; set; }
+        public bool isLive { get; set; }
+        public int numberOfLiveNeighbors { get; set; }
+
+        //Constructor
+        public cell(int rowY, int columnX)
+        {
+            this.columnX = columnX;
+            this.rowY = rowY;
+            this.wasVisited = false;
+            this.isLive = false;
+            this.numberOfLiveNeighbors = 0;
+        }
+
+       /* //Getter and Setter methods for properties
+        public int ColumnX { get => columnX; set => columnX = value; }
+        public int RowY { get => rowY; set => rowY = value; }
+        public bool WasVisited { get => wasVisited; set => wasVisited = value; }
+        public bool IsLive { get => isLive; set => isLive = value; }
+        public int NumberOfLiveNeighbors { get => numberOfLiveNeighbors; set => numberOfLiveNeighbors = value; }
+        */
+    }
+}
