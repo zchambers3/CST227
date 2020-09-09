ef283009f26004091431435c6dd8e4ca565d5f2d
zCoder <40376705+zchambers3@users.noreply.github.com>
2020-09-08 18:17:14 -0700

    Add files via upload

---
+++ CST_227_Activity5-master/StopWatch/ScoreList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    static public class ScoreList
+    {
+        static public List<String> scoreList = new List<String>();
+    }
+}
